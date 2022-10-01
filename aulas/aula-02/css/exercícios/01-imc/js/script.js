b51af666ea17ce21e3f0f65925b869d07fa8d916
main <francoschneider7@gmail.com>
2022-10-01 11:57:13 -0300

    aula 3, exercícios para fazer

--- aulas/aula-02/css/exercícios/01-imc/js/script.js
+++ aulas/aula-02/css/exercícios/01-imc/js/script.js
@@ -6,10 +6,34 @@ function calcularIMC() {
     const peso = document.getElementById('peso').value;
     const altura = document.getElementById('altura').value;
     const resultado = document.getElementById('resultado');
-    
+
+    //validação dos dados de entrada
+    if (!peso) {
+        return feedback(resultado, 'erro', 'Peso deve ser informado.');
+    }
+    if (!altura) {
+        return feedback(resultado, 'erro', 'Altura deve ser informada.');
+    }
     //realizar calculo
-    const imc = peso / (altura*altura);
+    const imc = peso / (altura * altura);
 
     //Apresentação do resultado
-    console.log (imc);
+    return feedback(resultado, 'sucesso', imc);
+}
+
+
+function feedback(elemento, tipo, mensagem) {
+    if (tipo == 'erro') {
+        elemento.classList.remove('sucesso');
+        elemento.classList.add('erro');
+        elemento.innerHTML = `
+            <p>${mensagem}</p>
+        `;
+    } else {
+        elemento.classList.remove('erro');
+        elemento.classList.add('sucesso');
+        elemento.innerHTML = `
+            <p>Resultado: ${mensagem}</p>
+        `;
+    }
 }
