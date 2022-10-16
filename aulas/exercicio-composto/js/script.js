28405e064a5465756bfab68571fbafc0f67821f9
FSCKOH <francisco.schneider85@gmail.com>
2022-10-16 17:27:29 -0300

    arquivos

---
+++ aulas/exercicio-composto/js/script.js
@@ -0,0 +1,11 @@
+function imc() {
+    
+    let nome = document.getElementById('nome').value;
+    let peso = document.getElementById('peso').value;
+    let altura = document.getElementById('altura').value;
+    console.log(nome);
+    console.log(peso);
+    console.log(altura);
+    
+    alert('Ola, '+nome+' seja bem vindo'+peso+altura)
+}
