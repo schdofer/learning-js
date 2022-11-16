3a4bccc800c72f4eb05eb418edad20c799f1d2b8
nataliaschmidt <nataliak.schmidt@gmail.com>
2022-11-16 10:57:52 -0300

    WIP: Realizando testes com ExpReg

--- aulas/aula_06/exercicios/js/script.js
+++ aulas/aula_06/exercicios/js/script.js
@@ -4,13 +4,9 @@ let idade = document.getElementById("idade").value;
 let telefone = document.getElementById("telefone").value;
 let email = document.getElementById("email").value;
 
-function realizarCadastro(){
+
     this.nome = nome;
     this.cpf = cpf;
     this.idade = document.getElementById("idade").value;
     this.telefone =document.getElementById("telefone").value;
     this.email = document.getElementById("email").value;
-
-    let cadastro = new Cadastro(nome, cpf, idade, telefone, email)
-    console.log(cadastro)
-}
---
+++ aulas/aula_07-regexp/exercicios/esqueci-a-senha/js/script.js
@@ -0,0 +1,11 @@
+
+function cadastrarCpf(){
+    // substituir o cpf por *
+    const cpf = document.getElementById('cpf').value;
+    const email = document.getElementById('email').value;
+    const substituiCpf = cpf.replace(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, '***.***.***-**');
+    const substituiEmail = email.replace(/^.+@\w+(\.\w{2,3}){1,2}$/, 'seu email')
+    // const substituiEmail = email.replace(/(.+)@(\w+)(\.\w{2,4})+/, 'seu email')
+
+    console.log(substituiEmail)
+}
