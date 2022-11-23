ed31ff7b41e0e2bd9c04cb84330a2dce6671a136
nataliaschmidt <nataliak.schmidt@gmail.com>
2022-11-23 16:06:39 -0300

    Esqueci minha senha com CSS

--- aulas/aula_07-regexp/exercicios/esqueci-a-senha/teste/script.js
+++ aulas/aula_07-regexp/exercicios/esqueci-a-senha/teste/script.js
@@ -1,36 +1,28 @@
-// verificação de senha
-// Deve conter pelo menos um dígito numérico
-// Deve contar pelo menos uma letra minúscula
-// Deve conter pelo menos uma letra maiúscula
-// Deve conter pelo menos um simbolo especial entre: $_.!#%&*()[]{}^~
-// Deve conter no mínimo 8 caracteres
-
-
 function cadastrarSenha() {
     let senha1 = document.getElementById('senha1').value;
     let senha2 = document.getElementById('senha2').value;
-    let regExp = /^(?=.*[$_\.!#%&\*()[\]{}\^~])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$_\.!#%&\*()[\]{}\^~])[0-9a-zA-Z$_\.!#%&\*()[\]{}\^~]{8,}$/
-    let confereSenha = regExp.test(senha1, senha2)
-    console.log(confereSenha)
+    let regExp = /^(?=.*[$_\.!#%&\*()[\]{}\^~])(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$_\.!#%&\*()[\]{}\^~])[0-9a-zA-Z$_\.!#%&\*()[\]{}\^~]{8,}$/;
+    let confereSenha = regExp.test(senha1, senha2);
+    console.log(confereSenha);
     if (senha1 === senha2) {
         try {
             if (confereSenha === true) {
-                return resultado.innerHTML = `nova senha cadastrada com sucesso`
+               alert("Nova senha cadastrada");
             }
             else if (confereSenha  === false) {
-                throw resultado.ineerHTML = `Senha fraca`
+                throw alert("Sua senha deve ter todos requisitos");
             }
         }
         catch (error) {
             resultado.innerHTML = error;
         }
         finally {
-            document.getElementById('senha1').value = ''
-            document.getElementById('senha2').value = ''
+            document.getElementById('senha1').value = '';
+            document.getElementById('senha2').value = '';
         }
     }
     else {
-        resultado.innerHTML = `As senhas são diferentes`
+        alert("As senhas devem ser iguais");
     }
 }
 
@@ -38,7 +30,5 @@ function cadastrarSenha() {
 
 //Não casa caracteres na posição atual, mas dá uma "espiada" adiante, e caso a ER embutida case, retorna sucesso.
 //	Matches any string that is followed by a specific string n
-// Pense que cada máscara entre parênteses é uma varredura individual em que o resultado da varredura deve ser TRUE quando fizer o match desejado.
-// A próxima máscara adicionada quase garante que todos os caracteres do texto sejam letras e números apenas. Sim, eu disse quase, mas ainda temos que garantir que a combinação desejada seja validada do início ao fim do texto, caso contrário as extremidades (bordas) do texto poderão conter caracteres indesejadados.
-//Agora sim, o acento circunflexo ˆ garante que a validação vai ser realizada a partir do primeiro caracter da senha e o dólar $ garante que a combinação desejada será feita até o último caracter da senha.
+// Pense que cada máscara entre parênteses é uma varredura individual em que o resultado da varredura deve ser TRUE quando fizer o match desejado
 
